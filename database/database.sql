93deb15ceb8e97bb32c71dea67be02486b684878
KhoaNguyen <khoanguyen@ubuntu.(none)>
2014-05-20 11:44:01 +0100

    Fix the database file. Add new row for locations, users. The file is now can be run on Postgre

--- database/database.sql
+++ database/database.sql
@@ -16,56 +16,69 @@ the database you may find it easiest to edit the definitions below and reload th
 
 --------------------------------------------------------------------------------------------------
 -- First we tidy up earlier versions of the tables if they exist to avoid conflicts.
-DROP TABLE Questions;
+DROP TABLE Question;
 DROP TABLE Users;
-DROP TABLE Requests;
-DROP TABLE Hints;
-DROP TABLE Locations;
+DROP TABLE Request;
+DROP TABLE Hint;
+DROP TABLE Location;
 
 --------------------------------------------------------------------------------------------------
--- Next we create the tables for our database.
+
+/*
+-- Hints table --
+Contains the information about the hints. There will be one row for each hint.
+*/
+CREATE TABLE Hint(
+  HintID integer Primary Key, --unique hint ID
+  Content text --the content of the hint 
+);
+
+/*
+-- Locations table --
+Contains the information about the locations. There will be one row for each hint.
+*/
+CREATE TABLE Location(
+  LocationID integer Primary Key, --unique location ID
+  x double precision NOT NULL, --X coordinate
+  y double precision NOT NULL, --Y coordinate
+  Name text --the name of the place 
+);
+
 /*
 -- Users table --
 Contains the information about the user. There will be one row for each user.
 */
-CREATE TABLE Users (
+CREATE TABLE Users(
   UserID integer Primary Key, --unique user ID
-  UserName text not null UNIQUE, --unique username for each user
-  UserPassword text not null UNIQUE, --unique password for user
-  Credits double precision NOT NULL,
-  QuestionID integer[] References Questions --list of question for that user 
+  UserName text unique not null, --unique username for each user
+  UserPassword text unique not null, --unique password for user
+  Credits double precision not null DEFAULT 10, --credit of the user 
+  QuestionID integer[], --list of questions for the user 
+  AnsweredQuestionID integer[], --list of questions that have been answered by the user  
+  LocationID integer[] --list of locations the user has checked in 
 );
 
 /*
 -- Questions table --
 Contains the information about the questions. There will be one row for each question.
 */
-CREATE TABLE Questions (
+CREATE TABLE Question(
   QuestionID integer Primary Key, --unique question ID
   Content text, --the content of the question
-  Makers integer References Users, --user ID of the user who made the question, default is admin
-  HintID integer[] References Hints, --list of hints for the question
-  Locations double precision[][2], --the location corresponses to the hints
+  Makers integer, --user ID of the user who made the question, default is admin
+  HintID integer[], --list of hints for the question
+  Locations integer[], --the location corresponses to the hints
   Ranking integer --ranking of the question in leaderboard  
-);
-
-/*
--- Hints table --
-Contains the information about the hints. There will be one row for each hint.
-*/
-CREATE TABLE Hints(
-  HintID integer Primary Key, --unique hint ID
-  Content text --the content of the hint 
 );
 
 /*
 -- Requests table --
 Contains the information about the requests. There will be one row for each request.
 */
-CREATE TABLE Requests(
-  QuestionID integer NOT null References Questions, --the ID of the question
-  RequesterID integer NOT null References Users, --the ID of the user who sent request
-  RequestForID integer NOT null References Users, --the ID of the user who received the request
+CREATE TABLE Request(
+  QuestionID integer NOT null, --the ID of the question
+  RequesterID integer NOT null, --the ID of the user who sent request
+  RequestForID integer NOT null, --the ID of the user who received the request
   Primary Key(QuestionID,RequesterID,RequestForID) --primary key to ensure no duplication of information 
 );
 
