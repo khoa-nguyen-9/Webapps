d3444916c5330a117948c1b977e37d197d3006ce
KhoaNguyen <khoanguyen@ubuntu.(none)>
2014-05-22 10:24:16 +0100

    update the db_config file to the current Postgre db

--- php/db_config.php
+++ php/db_config.php
@@ -4,8 +4,10 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "androidhive"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_HOST', "db.doc.ic.ac.uk"); // db host
+define('DB_PORT', "5432");            // db port number
+define('DB_DBNAME', "g1327113_u");    // db name
+define('DB_USER', "g1327113_u");      // db user
+define('DB_PASSWORD', "S24DKllQnP");  // db password
+
 ?>
