a31a98705b8975c211206a788cc4cbfd6566fa04
Astenne <corinaniculae3@gmail.com>
2014-05-26 16:38:37 +0100

    Added create table functions for the DatabaseHandler.

--- sampleApp/puzzle/src/library/DatabaseHandler.java
+++ sampleApp/puzzle/src/library/DatabaseHandler.java
@@ -13,6 +13,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
  
 public class DatabaseHandler extends SQLiteOpenHelper {
  
