6e6968a68d05a0bd8344e6d4f6c32752496fac20
KhoaNguyen <khoanguyen@ubuntu.(none)>
2014-05-26 17:34:32 +0100

    add basic fields to Request class

--- sampleApp/puzzle/src/com/webapps/puzzle/Request.java
+++ sampleApp/puzzle/src/com/webapps/puzzle/Request.java
@@ -1,5 +1,7 @@
 package com.webapps.puzzle;
 
 public class Request {
-	
+	private int requesterID;	// ID of the person who made the request	
+	private int requestedID;	// ID of the person who received the request
+	private int questID;		// ID of the question of the request
 }
