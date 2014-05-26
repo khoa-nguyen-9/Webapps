d28379bdb7763ab9b20fa2f2b7d56187937c6481
KhoaNguyen <khoanguyen@ubuntu.(none)>
2014-05-26 14:48:40 +0100

    make comments to the Question class

--- sampleApp/puzzle/src/com/webapps/puzzle/Question.java
+++ sampleApp/puzzle/src/com/webapps/puzzle/Question.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Question {
 	private User maker;				// The user who make the question
 	private ArrayList<Hint> hints;  // The list of hints for the question
-	private String content;			// The content of the questio
+	private String content;			// The content of the question
 	private int ranking; 			// The ranking of the question on leaderboard
 	
 	
@@ -15,20 +15,24 @@ public class Question {
 		this.content = content;
 	}
 	
+	// Get the user who made the question
 	public User getMaker() {
 		return maker;
 	}
 	
+	// Get the content of the question
 	public String getContent() {
 		return content;
 	}
 	
+	// Get the rank of the question on leader board
 	public int getRank() {
 		return ranking;
 	}
 	
-	public  ArrayList<Hint> getHints(int[] hintIDs) {
-		return null;
+	// Get list of hints for the question
+	public  ArrayList<Hint> getHints() {
+		return hints;
 	}
 	
 	// TODO: write a class to get the hint data
