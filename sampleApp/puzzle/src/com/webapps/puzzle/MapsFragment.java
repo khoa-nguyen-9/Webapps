b59f7971fb044696da3086ba576897e012039f2e
dkn12 <dkn12@ic.ac.uk>
2014-05-28 15:57:18 +0100

    remove sampleApp folder

--- sampleApp/puzzle/src/com/webapps/puzzle/MapsFragment.java
+++
@@ -1,7 +0,0 @@
-package com.webapps.puzzle;
-
-import android.support.v4.app.Fragment;
-
-public class MapsFragment extends Fragment {
-
-}
--- sampleApp/puzzle/src/com/webapps/puzzle/Request.java
+++
@@ -1,7 +0,0 @@
-package com.webapps.puzzle;
-
-public class Request {
-	private int requesterID;	// ID of the person who made the request	
-	private int requestedID;	// ID of the person who received the request
-	private int questID;		// ID of the question of the request
-}
