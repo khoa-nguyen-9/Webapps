ecfeb86ce74fbb9691d3c61fd5ea6ff245715d30
Astenne <corinaniculae3@gmail.com>
2014-06-03 15:23:08 +0100

    Added about and contact pages.

---
+++ php/about.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("../website/index.html");
+
+echo "This is a second year project done by Imperial students as part of their summer term project."
+
+?>
---
+++ php/contact.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../website/index.html");
+
+echo "To be completed.";
+
+$form = <<<EOF
+<br>
+<i> Form to be added </i>
+EOF;
+
+echo "$form";
+
+?>
